8fbb15191ae92f84064bb204317b2b4aa9fb5fe3
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-10-18 12:37:22 +0200

    Pobieranie od uzytkownika niepoprawnego stringa

--- zpi_lab2/src/main/java/zpi_lab2/zpi_lab2/App.java
+++ zpi_lab2/src/main/java/zpi_lab2/zpi_lab2/App.java
@@ -6,13 +6,18 @@ public class App {
 	public static void main(String[] args) {
 		String fileName;
 		String corectString;
+		String incorrectString;
+
 		Scanner scan = new Scanner(System.in);
 
 		System.out.print("Podaj nazwe pliku: ");
 		fileName = scan.nextLine();
 
-		System.out.print("Podaj nazwe pliku: ");
+		System.out.print("Podaj poprawny string: ");
 		corectString = scan.nextLine();
+
+		System.out.print("Podaj niepoprawny string: ");
+		incorrectString = scan.nextLine();
 
 	}
 }
