71e6d774e4461c9d73ca3d3e87a533db8a80f259
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-10-18 12:36:15 +0200

    Pobieranie od uzytkownika poprawnego stringa

--- zpi_lab2/src/main/java/zpi_lab2/zpi_lab2/App.java
+++ zpi_lab2/src/main/java/zpi_lab2/zpi_lab2/App.java
@@ -5,8 +5,14 @@ import java.util.Scanner;
 public class App {
 	public static void main(String[] args) {
 		String fileName;
+		String corectString;
 		Scanner scan = new Scanner(System.in);
+
 		System.out.print("Podaj nazwe pliku: ");
 		fileName = scan.nextLine();
+
+		System.out.print("Podaj nazwe pliku: ");
+		corectString = scan.nextLine();
+
 	}
 }
