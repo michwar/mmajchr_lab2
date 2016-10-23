18a675e46e8c656c18510b801b4593d897ea2c39
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-10-23 19:04:28 +0200

    Dodanie construktora do klasy FileOperation przyjmujacego FirstCallback

--- zpi_lab2/src/main/java/zpi_lab2/zpi_lab2/FileOperation.java
+++ zpi_lab2/src/main/java/zpi_lab2/zpi_lab2/FileOperation.java
@@ -1,8 +1,16 @@
 package zpi_lab2.zpi_lab2;
 
+import lab2lib.lab2lib.FirstCallback;
+
 public class FileOperation {
 
+	private FirstCallback mFirstCallback;
+
 	public FileOperation() {
+	}
+
+	public FileOperation(FirstCallback firstCallback) {
+		mFirstCallback = firstCallback;
 	}
 
 	public void saveFile(String fileName, String correctString, String incorrectString) {
