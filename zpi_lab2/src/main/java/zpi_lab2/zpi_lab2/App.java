ff5f81285f806a0211d27480aad592c901cbdc1d
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-10-23 19:04:52 +0200

    Utworzenie obiektu FileOperation i wywolanie metody do zapisu pliku

--- zpi_lab2/src/main/java/zpi_lab2/zpi_lab2/App.java
+++ zpi_lab2/src/main/java/zpi_lab2/zpi_lab2/App.java
@@ -3,6 +3,7 @@ package zpi_lab2.zpi_lab2;
 import java.util.Scanner;
 
 import lab2lib.lab2lib.FirstCallback;
+import lab2lib.lab2lib.FirstCallbackImpl;
 
 public class App {
 	public static void main(String[] args) {
@@ -21,7 +22,9 @@ public class App {
 		System.out.print("Podaj niepoprawny string: ");
 		incorrectString = scan.nextLine();
 		
-		FirstCallback firstCallback;
-		FileOperation fileOperation = new FileOperation();
+		FirstCallback firstCallback = new FirstCallbackImpl(scan);
+		FileOperation fileOperation = new FileOperation(firstCallback);
+		fileOperation.saveFile(fileName, corectString, incorrectString);
+		
 	}
 }
