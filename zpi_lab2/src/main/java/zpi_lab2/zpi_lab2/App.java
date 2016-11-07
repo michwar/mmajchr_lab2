90b87de30b1ad55774297a00d63eda2cd3594410
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-11-07 16:13:53 +0100

    Przeniesienie kodu z App.start do Main.run

--- zpi_lab2/src/main/java/zpi_lab2/zpi_lab2/App.java
+++ zpi_lab2/src/main/java/zpi_lab2/zpi_lab2/App.java
@@ -2,8 +2,6 @@ package zpi_lab2.zpi_lab2;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
-import lab2lib.lab2lib.FirstCallback;
-import lab2lib.lab2lib.FirstCallbackImpl;
 
 public class App extends Application {
 
@@ -13,21 +11,7 @@ public class App extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		String fileName;
-		String corectString;
-		String incorrectString;
-
-		SimpleConsole ui = SimpleConsole.getInstance();
-
-		fileName = ui.prompt("Podaj nazwe pliku: ");
-
-		corectString = ui.prompt("Podaj poprawny string: ");
-
-		incorrectString = ui.prompt("Podaj niepoprawny string: ");
-
-		FirstCallback firstCallback = new FirstCallbackImpl(ui);
-		FileOperation fileOperation = new FileOperation(firstCallback, ui);
-		fileOperation.saveFile(fileName, corectString, incorrectString);
+		;
 	}
 
 }
--- zpi_lab2/src/main/java/zpi_lab2/zpi_lab2/Main.java
+++ zpi_lab2/src/main/java/zpi_lab2/zpi_lab2/Main.java
@@ -1,9 +1,27 @@
 package zpi_lab2.zpi_lab2;
 
+import lab2lib.lab2lib.FirstCallback;
+import lab2lib.lab2lib.FirstCallbackImpl;
+
 public class Main implements Runnable {
 
 	@Override
 	public void run() {
+		String fileName;
+		String corectString;
+		String incorrectString;
+
+		SimpleConsole ui = SimpleConsole.getInstance();
+
+		fileName = ui.prompt("Podaj nazwe pliku: ");
+
+		corectString = ui.prompt("Podaj poprawny string: ");
+
+		incorrectString = ui.prompt("Podaj niepoprawny string: ");
+
+		FirstCallback firstCallback = new FirstCallbackImpl(ui);
+		FileOperation fileOperation = new FileOperation(firstCallback, ui);
+		fileOperation.saveFile(fileName, corectString, incorrectString);
 	}
 
 }
