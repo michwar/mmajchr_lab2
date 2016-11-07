1de3c8007fc2fa821652af62104ccfd1e6a23d31
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-11-07 17:11:45 +0100

    onCloseCallback ustawiany z zewnatrz

--- zpi_lab2/src/main/java/zpi_lab2/zpi_lab2/App.java
+++ zpi_lab2/src/main/java/zpi_lab2/zpi_lab2/App.java
@@ -4,9 +4,10 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.stage.Stage;
 import lab2lib.lab2lib.OnCloseCallback;
-import lab2lib.lab2lib.OnCloseCallbackImpl;
 
 public class App extends Application {
+	
+	private OnCloseCallback onCloseCallback;
 
 	public static void main(String[] args) {
 		launch(args);
@@ -19,11 +20,18 @@ public class App extends Application {
 		runMain();
 	}
 	
+	public OnCloseCallback getOnCloseCallback() {
+		return onCloseCallback;
+	}
+
+	public void setOnCloseCallback(OnCloseCallback onCloseCallback) {
+		this.onCloseCallback = onCloseCallback;
+	}
+
 	private void setExplicitExit(Stage primaryStage) {
-		OnCloseCallback onCloseCallback = new OnCloseCallbackImpl(SimpleConsole.getInstance());
 		Platform.setImplicitExit(false);
 		primaryStage.setOnCloseRequest(ev -> {
-			if(onCloseCallback.closing()) {
+			if(onCloseCallback == null || onCloseCallback.closing()) {
 				Platform.exit();
 			} else {
 				ev.consume();
