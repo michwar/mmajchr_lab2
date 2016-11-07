2058dd61d51b722fdeaaf2ad1bdf5957568ed981
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-11-07 15:19:52 +0100

    SimpleConsole jako singleton.

--- zpi_lab2/src/main/java/zpi_lab2/zpi_lab2/SimpleConsole.java
+++ zpi_lab2/src/main/java/zpi_lab2/zpi_lab2/SimpleConsole.java
@@ -3,17 +3,20 @@ package zpi_lab2.zpi_lab2;
 import lab2lib.lab2lib.SimpleUserInterface;
 
 public class SimpleConsole implements SimpleUserInterface {
+	
+	private static SimpleConsole instance = new SimpleConsole();
+	
+	public static SimpleConsole getInstance() {
+		return instance;
+	}
 
 	@Override
 	public String prompt(String arg0) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public void show(String arg0) {
-		// TODO Auto-generated method stub
-
 	}
 
 }
