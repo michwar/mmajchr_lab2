8e10bf27d9984b4d79a21b6643d03a8d3116b43b
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-11-07 17:15:46 +0100

    metody getOnCloseCallback i setOnCloseCallback w interfejsie Close

--- zpi_lab2/src/main/java/zpi_lab2/zpi_lab2/Close.java
+++ zpi_lab2/src/main/java/zpi_lab2/zpi_lab2/Close.java
@@ -1,5 +1,11 @@
 package zpi_lab2.zpi_lab2;
 
+import lab2lib.lab2lib.OnCloseCallback;
+
 public interface Close {
+
+	public OnCloseCallback getOnCloseCallback();
+
+	public void setOnCloseCallback(OnCloseCallback onCloseCallback);
 
 }
