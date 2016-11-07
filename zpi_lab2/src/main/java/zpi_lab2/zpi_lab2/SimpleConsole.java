4655245f9d46b9302c1fe4fc12f68cc2b509aaa8
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-11-07 15:17:06 +0100

    Klasa SimpleConsole implementujaca interface SimpleUserInterface.

---
+++ zpi_lab2/src/main/java/zpi_lab2/zpi_lab2/SimpleConsole.java
@@ -0,0 +1,19 @@
+package zpi_lab2.zpi_lab2;
+
+import lab2lib.lab2lib.SimpleUserInterface;
+
+public class SimpleConsole implements SimpleUserInterface {
+
+	@Override
+	public String prompt(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void show(String arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
